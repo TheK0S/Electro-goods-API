71fc4dcd68703fef4bc2d2a43c970910ac248a78
Konstantin <turchakkonstantin@gmail.com>
2023-11-28 16:34:17 +0200

    add models: User, Category, Role

---
+++ Electro-goods-API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Electro_goods_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronomic { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateOnly Birthdate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
