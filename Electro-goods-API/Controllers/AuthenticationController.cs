9659d2a4c1be0a1c308571e98a712bd8f759e7cc
Константин <turchakkonstantin@gmail.com>
2024-06-06 16:44:29 +0300

    added register method in AuthController

--- Electro-goods-API/Controllers/AuthenticationController.cs
+++ Electro-goods-API/Controllers/AuthenticationController.cs
@@ -1,6 +1,7 @@
 ﻿using Electro_goods_API.Exceptions;
 using Electro_goods_API.Helpers;
 using Electro_goods_API.Models.DTO;
+using Electro_goods_API.Models.Entities;
 using Electro_goods_API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,11 +13,13 @@ namespace Electro_goods_API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IAuthenticationRepository _authRepository;
+        private readonly IBasketRepository _basketRepository;
 
-        public AuthenticationController(IUserRepository userRepository, IAuthenticationRepository authRepository)
+        public AuthenticationController(IUserRepository userRepository, IAuthenticationRepository authRepository, IBasketRepository basketRepository)
         {
             _userRepository = userRepository;
             _authRepository = authRepository;
+            _basketRepository = basketRepository;
         }
 
         //POST: api/Auth/Login
@@ -37,6 +40,32 @@ namespace Electro_goods_API.Controllers
             string jwtToken = _authRepository.GenerateJwtToken(user.Id, user.Email, user.Role.Name);
 
             return Ok(jwtToken);
+        }
+        //POST: api/Auth/Register
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO registerRequest)
+        {
+            if (registerRequest.Password != registerRequest.ConfirmPassword)
+                throw new ArgumentException("Пароли не равны");
+
+            User newUser = new()
+            {
+                FirstName = registerRequest.FirstName,
+                LastName = registerRequest.LastName,
+                Patronomic = registerRequest.Patronomic,
+                Email = registerRequest.Email,
+                Password = HashPasswordHelper.HashPasword(registerRequest.Password),
+                PhoneNumber = registerRequest.PhoneNumber,
+                Birthdate = registerRequest.Birthdate ?? DateTime.Now.Date,
+                CreationDate = DateTime.Now,
+                IsActive = true,
+                RoleId = 2,                
+            };
+
+            User registeredUser = await _userRepository.CreateUser(newUser);
+            await _basketRepository.CreateBasketByUserId(registeredUser.Id);
+
+            return Ok();
         }
 
         //DELETE: api/Auth/Logout
--- Electro-goods-API/Models/DTO/RegisterDTO.cs
+++ Electro-goods-API/Models/DTO/RegisterDTO.cs
@@ -2,5 +2,14 @@
 {
     public class RegisterDTO
     {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Patronomic { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime? Birthdate { get; set; }
     }
 }
